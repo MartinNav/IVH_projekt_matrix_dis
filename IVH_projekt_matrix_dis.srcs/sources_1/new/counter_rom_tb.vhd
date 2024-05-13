e64a409d981bb680a35a1bad9994d96bd16824c1
MartinNav <martinnavratilu+git@gmail.com>
2024-05-13 17:16:56 +0200

    rom test is passing

--- IVH_projekt_matrix_dis.srcs/sources_1/new/counter_rom_tb.vhd
+++ IVH_projekt_matrix_dis.srcs/sources_1/new/counter_rom_tb.vhd
@@ -34,7 +34,7 @@ use xil_defaultlib.all;
 --use UNISIM.VComponents.all;
 
 entity counter_rom_tb is
---  Port ( );
+  Port ( clk:in std_logic);
 end counter_rom_tb;
 
 architecture Behavioral of counter_rom_tb is
@@ -53,32 +53,34 @@ process(clk)
 begin
     if rising_edge(clk) then
     -- these values may seem weird but they are not it is because I am using little endian to represen the display instead of big endian
+    -- v mojí implementaci není ROM oddìlená od èítaèù
         case ctr is
-            when 0=> 
-                assert output = x"7c447c007c447c00" report "case 0 does not equal 00";
-            when 1=>
-                assert output = x"7c0000007c447c00" report "case 1 does not equal 01";
+           --starting from 2 because counter will take its time 
             when 2=>
-                assert output = x"74545c007c447c00" report "case 2 does not equal 02";
+                assert output = x"7c0000007c447c00" report "case 1 does not equal 01";
             when 3=>
-                assert output = x"7c5454007c447c00" report "case 3 does not equal 03";
+                assert output = x"74545c007c447c00" report "case 2 does not equal 02";
             when 4=>
-                assert output = x"7c1070007c447c00" report "case 4 does not equal 04";
+                assert output = x"7c5454007c447c00" report "case 3 does not equal 03";
             when 5=>
-                assert output = x"5c5474007c447c00" report "case 5 does not equal 05";
+                assert output = x"7c1070007c447c00" report "case 4 does not equal 04";
             when 6=>
-                assert output = x"5c547c007c447c00" report "case 6 does not equal 06";
+                assert output = x"5c5474007c447c00" report "case 5 does not equal 05";
             when 7=>
-                assert output = x"7c4040007c447c00" report "case 7 does not equal 07";
+                assert output = x"5c547c007c447c00" report "case 6 does not equal 06";
             when 8=>
-                assert output = x"7c547c007c447c00" report "case 8 does not equal 08";
+                assert output = x"7c4040007c447c00" report "case 7 does not equal 07";
             when 9=>
-                assert output = x"7c5070007c447c00" report "case 9 does not equal 09";
+                assert output = x"7c547c007c447c00" report "case 8 does not equal 08";
             when 10=>
-                assert output = x"7c447c007c000000" report "case 10 does not equal 10";
-                wait;
+                assert output = x"7c5070007c447c00" report "case 9 does not equal 09";
+            when 11=>
+                assert output = x"7c447c007c447c00" report "case 10 does not equal 10";
+           when 17=> 
+                assert output = x"5c547c007c000000" report "case 16 does not equal 16";
+                --wait;
             
-            
+            when others=>NULL;
             
            
         
